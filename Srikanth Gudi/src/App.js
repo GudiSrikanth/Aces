aec37cc274cce11f0f334a1e0690f92eb0138799
GudiSrikanth <91872346+GudiSrikanth@users.noreply.github.com>
2022-06-07 15:04:52 +0530

    commit by Srikanth Gudi

--- Srikanth Gudi/src/App.js
+++ Srikanth Gudi/src/App.js
@@ -6,40 +6,33 @@ import LoginPage from './components/pages/LoginPage'
 import RegisterPage from './components/pages/RegisterPage'
 import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
 import HomePage from './components/pages/HomePage'
-
+import Header from "./components/pages/header";
+import Footer from "./components/pages/Footer"
+import productDetail from './components/pages/productDetails'
+import Products from './components/pages/Products'
 import './App.css'
 
 export default function App() {
     return (
         <Router>
             <div>
+                <Header/>
+
                 <Switch>
                     <Route exact path="/" component={ LandingPage } />
                     <Route path="/login" component={ LoginPage } />
                     <Route path="/register" component={ RegisterPage } />
                     <Route path="/forget-password" component={ ForgetPasswordPage } />
                     <Route path="/home" component={ HomePage } />
+                    <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <productDetail />
+        </Route>
                 </Switch>
                 <Footer />
             </div>
         </Router>
     )
-}
-
-const Footer = () => {
-    return (
-        <p className="text-center" style={ FooterStyle }>Designed & coded by SG</p>
-    )
-}
-
-const FooterStyle = {
-    background: "#222",
-    fontSize: ".8rem",
-    color: "#fff",
-    position: "absolute",
-    bottom: 0,
-    padding: "1rem",
-    margin: 0,
-    width: "100%",
-    opacity: ".5"
 }
